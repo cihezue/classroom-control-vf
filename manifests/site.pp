272fd2911809bb77b159fb86d61807e6652266bf
cihezue <cihezue@actionet.com>
2016-07-27 14:32:29 -0400

    10.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,12 +38,4 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  notify { "Hello, my name is ${::cihezue}": }
-   
-   include users 
-   include skeleton
-  }
+

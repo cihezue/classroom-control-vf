c7be4d05aa424de9d7fa02e8ff3399612759bf39
cihezue <cihezue@actionet.com>
2016-07-28 10:47:14 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -36,4 +36,13 @@ service { 'nginx':
 ensure => running,
 enable => true,
  }
+ node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
+ }
 }
+

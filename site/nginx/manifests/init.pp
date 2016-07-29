7464460812664766e7c233ddca8abf9f8d047be7
cihezue <cihezue@actionet.com>
2016-07-29 12:35:08 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,13 @@
 class nginx (
-$root=undefs,
-){
-case $::osfamily {
-'redhat','debian' : {
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-$docroot = '/var/www'
-$confdir = '/etc/nginx'
-$logdir = '/var/log/nginx'
-}
-'windows' : {
-$package = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-$docroot = 'C:/ProgramData/nginx/html'
-$confdir = 'C:/ProgramData/nginx'
-$logdir = 'C:/ProgramData/nginx/logs'
-}
-default : {
-fail("Module ${module_name} is not supported on ${::osfamily}")
-}
-}
-# user the service will run as. Used in the nginx.conf.erb template
-$user = $::osfamily ? {
-'redhat' => 'nginx',
-'debian' => 'www-data',
-'windows' => 'nobody',
-}
+$package = $nginx::params::package,
+$owner   = $nginx::params::owner,
+$group   = $nginx::params::group,
+$docroot = $nginx::params::docroot,
+$confdir = $nginx::params::confdir,
+$logdir  = $nginx::params::logdir,
+$user    = $nginx::params::user,
+) inherits nginx::params {
+
 File {
 owner => $owner,
 group => $group,
@@ -52,10 +32,11 @@ file { "${confdir}/conf.d/default.conf":
 ensure => file,
 content => template('nginx/default.conf.erb'),
 notify => Service['nginx'],
- }
+}
 service { 'nginx':
 ensure => running,
 enable => true,
- }
-} 
+}
+}
+
 

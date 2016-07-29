059033f67c1e5ea8a7d59cafa0e08a4d85f3bc18
cihezue <cihezue@actionet.com>
2016-07-29 12:24:01 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,1 +1,10 @@
-
+class nginx::params {
+case $::osfamily {
+'redhat','debian' : {
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+$docroot = '/var/www'
+$confdir = '/etc/nginx'
+$logdir = '/var/log/nginx'
+}

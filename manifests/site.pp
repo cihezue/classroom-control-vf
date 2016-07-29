c86cca0f62445e832b87b5a5dd9c9e420adee51e
cihezue <cihezue@actionet.com>
2016-07-29 10:16:23 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,4 @@
 ## site.pp ##
-
 # This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
 #
@@ -47,7 +46,7 @@ notify { "This is a ${vmname} virtual machine.": }
  }
  node default {
 $message = hiera('message')
-notify { $message: }
+ notify { $message: }
  }
 }
 
